19932543fb992fbfe2e229a2f25150170a830d60
Данил Ченцов <danil090598@gmail.com>
2019-03-08 02:49:57 +0300

    connecting to DB via MongoAtlas

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-import com.mongodb.*;
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
@@ -8,23 +10,21 @@ import static com.mongodb.client.model.Filters.eq;
 public class Main {
 
     public static void main(String[] args) {
+
+        Block<Document> printBlock = document -> System.out.println(document.toJson());
         //Connecting
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb://stargen:<PASSWORD>@cluster0-shard-00-00-dkc5z.mongodb.net:27017,cluster0-shard-00-01-dkc5z.mongodb.net:27017,cluster0-shard-00-02-dkc5z.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
 
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("DB");
 
-        MongoDatabase database = mongoClient.getDatabase("testBase");
-        MongoCollection collection = database.getCollection("testCollection");
+        MongoCollection<Document> collection = database.getCollection("Dates");
 
-
-        //Read a Document from a Collection
-
-
-        Document myDoc = (Document) collection.find(eq("event_name", "TEST_EVENT")).first();
-        System.out.println(myDoc.toJson());
-
-
+        //Reading
+        collection.find(eq("district", " 8; Черноморский пер."))
+                .forEach(printBlock);
 
         mongoClient.close();
-
     }
 }
